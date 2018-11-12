27f3fdf90976e0620ea02373af142708701de62f
Maxim Yavlanskiy <yavlanskiy@gmail.com>
2018-11-12 18:23:55 +0200

    base

---
+++ src/main/com/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by max_yav on 12.11.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/com/noobchain/model/Block.java
@@ -0,0 +1,7 @@
+package noobchain.model;
+
+/**
+ * Created by max_yav on 12.11.2018.
+ */
+public class Block {
+}
