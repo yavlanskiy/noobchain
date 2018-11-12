db224492cc85b3412a34cb197af8cb3fcacc68ea
Maxim Yavlanskiy <yavlanskiy@gmail.com>
2018-11-12 18:43:18 +0200

    gson

--- src/main/com/noobchain/model/Block.java
+++ src/main/com/noobchain/model/Block.java
@@ -1,7 +1,16 @@
 package noobchain.model;
 
-/**
- * Created by max_yav on 12.11.2018.
- */
+import java.util.Date;
+
 public class Block {
+    public String hash;
+    public  String previousHash;
+    private String data; // our data will be a simple message
+    private  long timeStamp; //
+
+    public Block(String previousHash, String data) {
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timeStamp = new Date().getTime();
+    }
 }
